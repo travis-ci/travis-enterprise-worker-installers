23c600d6e5e751624fc4a40e799fd23db4a41326
Jan Schulte <jan@travis-ci.com>
2018-05-11 18:59:08 +0000

    Pull down build environment images

--- airgap/pull-images.sh
+++ airgap/pull-images.sh
@@ -5,6 +5,8 @@
 set -e
 export DEBIAN_FRONTEND=noninteractive
 
+export TRAVIS_WORKER_VERSION="v3.5.0"
+
 download_docker() {
   wget "https://apt.dockerproject.org/repo/pool/main/d/docker-engine/docker-engine_17.05.0~ce-0~ubuntu-xenial_amd64.deb"
 }
@@ -23,3 +25,32 @@ download_apt_packages() {
 }
 
 download_apt_packages
+
+prepare_images_folder() {
+  mkdir -p docker_images
+}
+
+prepare_images_folder
+
+pull_travis_worker() {
+  sudo docker pull travisci/worker:$TRAVIS_WORKER_VERSION
+  sudo docker save travisci/worker:$TRAVIS_WORKER_VERSION -o docker_images/travis-worker.tar
+}
+
+pull_travis_worker
+
+pull_trusty_build_images() {
+  echo "Installing Ubuntu Trusty build images"
+  image_mappings_json=$(curl https://raw.githubusercontent.com/travis-infrastructure/terraform-config/master/aws-production-2/generated-language-mapping.json)
+
+  docker_images=$(echo "$image_mappings_json" | jq -r "[.[]] | unique | .[]")
+
+  for docker_image in $docker_images; do
+    image_filename=$(echo $docker_image | sed 's/travisci\///')
+    sudo docker pull "$docker_image"
+    sudo docker save "$docker_image" -o docker_images/"$image_filename".tar
+  done
+
+}
+
+pull_trusty_build_images
