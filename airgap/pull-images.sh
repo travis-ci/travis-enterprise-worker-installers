32cb09de57d8d1655e196f1919a150d1377712e7
Jan Schulte <jan@travis-ci.com>
2018-05-11 17:33:03 +0000

    Download apt-get packages into local directory

--- airgap/pull-images.sh
+++ airgap/pull-images.sh
@@ -5,10 +5,10 @@
 set -e
 export DEBIAN_FRONTEND=noninteractive
 
-wget https://apt.dockerproject.org/repo/pool/main/d/docker-engine/docker-engine_17.05.0~ce-0~ubuntu-trusty_amd64.deb
+wget "https://apt.dockerproject.org/repo/pool/main/d/docker-engine/docker-engine_17.05.0~ce-0~ubuntu-xenial_amd64.deb"
+
 sudo apt-get update
-sudo apt-get install --download-only libsystemd-journal0 \ 
-	libltdl7 \
+sudo apt-get -d -o dir::cache=`pwd` -o Debug::NoLocking=1 install --download-only -y libltdl7 \
 	aufs-tools \
 	apt-transport-https \
 	ca-certificates \
