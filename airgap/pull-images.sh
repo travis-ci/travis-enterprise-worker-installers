45717e875983dc61f62bc8e522488ba96ce813bd
Jan Schulte <jan@travis-ci.com>
2018-05-11 17:59:41 +0000

    Put steps into functions

--- airgap/pull-images.sh
+++ airgap/pull-images.sh
@@ -5,14 +5,21 @@
 set -e
 export DEBIAN_FRONTEND=noninteractive
 
-wget "https://apt.dockerproject.org/repo/pool/main/d/docker-engine/docker-engine_17.05.0~ce-0~ubuntu-xenial_amd64.deb"
+download_docker() {
+  wget "https://apt.dockerproject.org/repo/pool/main/d/docker-engine/docker-engine_17.05.0~ce-0~ubuntu-xenial_amd64.deb"
+}
 
-sudo apt-get update
-sudo apt-get -d -o dir::cache=`pwd` -o Debug::NoLocking=1 install --download-only -y libltdl7 \
-	aufs-tools \
-	apt-transport-https \
-	ca-certificates \
-	curl \
-	software-properties-common \
-	jq
+download_docker
 
+download_apt_packages() {
+	sudo apt-get update
+	sudo apt-get -d -o dir::cache=`pwd` -o Debug::NoLocking=1 install --download-only -y libltdl7 \
+		aufs-tools \
+		apt-transport-https \
+		ca-certificates \
+		curl \
+		software-properties-common \
+		jq
+}
+
+download_apt_packages
