909689c82f50be62881a3825a86086100bb86366
Jan Schulte <jan@travis-ci.com>
2018-05-14 16:55:34 +0000

    Docker and apt packages need to be pre-installed

--- airgap/pull-images.sh
+++ airgap/pull-images.sh
@@ -15,31 +15,6 @@ prepare_work_directory
 
 cd workdir
 
-download_docker() {
-  wget "https://apt.dockerproject.org/repo/pool/main/d/docker-engine/docker-engine_17.05.0~ce-0~ubuntu-xenial_amd64.deb"
-}
-
-download_docker
-
-download_apt_packages() {
-	sudo apt-get update
-	sudo apt-get -d -o dir::cache=`pwd` -o Debug::NoLocking=1 install --download-only -y libltdl7 \
-		aufs-tools \
-		apt-transport-https \
-		ca-certificates \
-		curl \
-		software-properties-common \
-		jq
-}
-
-# download_apt_packages
-
-zip_apt_packages() {
-  tar -zcvf system-packages.tar.gz -C archives .
-}
-
-# zip_apt_packages
-
 prepare_images_folder() {
   mkdir -p docker_images
 }
