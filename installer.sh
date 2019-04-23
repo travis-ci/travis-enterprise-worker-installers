4bb544c8df3e483e7c4e44836f72b8d59087a741
Danish Khan <danish@travis-ci.com>
2019-04-23 16:28:36 -0700

    Updated Opal and Sardonyx images for Xenial

--- installer.sh
+++ installer.sh
@@ -283,8 +283,8 @@ pull_trusty_build_images() {
 pull_xenial_build_images() {
   echo "Installing Ubuntu 16.04 build images"
 
-  opal=travisci/ci-opal:packer-1547455612-2c98a19
-  sardonyx=travisci/ci-sardonyx:packer-1547455648-2c98a19
+  opal=travisci/ci-opal:packer-1554885370-f909ac5
+  sardonyx=travisci/ci-sardonyx:packer-1554885359-f909ac5
 
   docker pull $opal
   docker pull $sardonyx
