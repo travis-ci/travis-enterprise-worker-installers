2a778aa3aaacdadba26c4927afb5f6e6e4ee2fee
Maciej Kempin <m.kempin@livelinktechnology.net>
2021-08-25 16:56:27 +0100

    travis-worker works

--- lxd/lxd_install.sh
+++ lxd/lxd_install.sh
@@ -2,19 +2,31 @@
 set -eux
 
 # functions
-dump_worker_config_file() {
-cat > $TRAVIS_WORKER_CONFIG_FILE_PATH<<- EOM
-TRAVIS_ENTERPRISE_SECURITY_TOKEN="${TRAVIS_ENTERPRISE_SECURITY_TOKEN}"
-TRAVIS_ENTERPRISE_BUILD_ENDPOINT="${TRAVIS_ENTERPRISE_BUILD_ENDPOINT}"
-TRAVIS_BUILD_IMAGES="${TRAVIS_BUILD_IMAGES}"
-TRAVIS_QUEUE_NAME="${TRAVIS_QUEUE_NAME}"
-EOM
+export_travis_worker_config() {
+export TRAVIS_ENTERPRISE_SECURITY_TOKEN="${TRAVIS_ENTERPRISE_SECURITY_TOKEN}"
+export TRAVIS_ENTERPRISE_BUILD_ENDPOINT="${TRAVIS_ENTERPRISE_BUILD_ENDPOINT}"
+export TRAVIS_BUILD_IMAGES="${TRAVIS_BUILD_IMAGES}"
+export TRAVIS_QUEUE_NAME="${TRAVIS_QUEUE_NAME}"
+export TRAVIS_ENTERPRISE_HOST="${TRAVIS_ENTERPRISE_HOST}"
+export BUILD_API_URI="https://${TRAVIS_ENTERPRISE_HOST:-localhost}/${TRAVIS_ENTERPRISE_BUILD_ENDPOINT:-__build__}/script"
+export TRAVIS_WORKER_DOCKER_NATIVE="true"
+export AMQP_URI="amqp://travis:${TRAVIS_ENTERPRISE_SECURITY_TOKEN:-travis}@${TRAVIS_ENTERPRISE_HOST:-localhost}/travis"
+export TRAVIS_WORKER_DOCKER_NATIVE="true"
+export TRAVIS_WORKER_BUILD_API_INSECURE_SKIP_VERIFY='true'
+export POOL_SIZE='2'
+export PROVIDER_NAME='docker'
+export TRAVIS_WORKER_DOCKER_ENDPOINT='unix:///var/run/docker.sock'
+export SILENCE_METRICS="true"
+export TRAVIS_WORKER_DOCKER_BINDS="/sys/fs/cgroup:/sys/fs/cgroup"
+export TRAVIS_WORKER_DOCKER_SECURITY_OPT="seccomp=unconfined"
+export TRAVIS_WORKER_DOCKER_TMPFS_MAP="/run:rw,nosuid,nodev,exec,noatime,size=65536k+/run/lock:rw,nosuid,nodev,exec,noatime,size=65536k"
+export QUEUE_NAME="${TRAVIS_QUEUE_NAME}"
 }
 
 
 # consts
 TRAVIS_LXD_INSTALL_SCRIPT_IMAGE_URL=https://travis-lxc-images.s3.us-east-2.amazonaws.com
-TRAVIS_WORKER_CONFIG_FILE_PATH="/var/snap/travis-worker/common/worker.env"
+TRAVIS_WORKER_CONFIG_FILE_PATH="/etc/default/travis-worker"
 declare -A TRAVIS_LXD_INSTALL_SCRIPT_IMAGES_MAP=( ["amd64-focal"]="travis-ci-ubuntu-2004-1603734892-1fb6ced8.tar.gz" )
 
 
@@ -56,10 +68,6 @@ TRAVIS_QUEUE_NAME="${TRAVIS_QUEUE_NAME:-builds.bionic}"
 TRAVIS_BUILD_IMAGES="${TRAVIS_BUILD_IMAGES:-focal}"
 
 
-TRAVIS_WORKER_HARD_TIMEOUT="90m"
-TRAVIS_WORKER_DOCKER_PRIVILEGED="true"
-TRAVIS_WORKER_DOCKER_CPUS=4
-
 
 
 if [[ ! -v TRAVIS_ENTERPRISE_SECURITY_TOKEN ]]; then
@@ -94,7 +102,8 @@ snap set lxd shiftfs.enable=true
 # install travis worker binary
 snap install travis-worker --edge
 snap connect travis-worker:lxd lxd:lxd
-dump_worker_config_file
+mkdir -p /etc/default
+export_travis_worker_config
 
 # the user can use image name or just pass architecture and linux distro so TRAVIS_LXD_INSTALL_SCRIPT_IMAGES_MAP can resolve the image name
 if [[ -v TRAVIS_LXD_INSTALL_SCRIPT_IMAGE ]]; then
@@ -110,8 +119,6 @@ else
   curl "${TRAVIS_LXD_INSTALL_SCRIPT_IMAGE_URL}/${TRAVIS_LXD_INSTALL_SCRIPT_IMAGE}" --output $image_file
 fi
 
-
-
 # installing the image
 
 lxc image import $image_file --alias travis-image
@@ -123,4 +130,4 @@ lxc launch travis-image travis-container
 
 lxc config device add travis-container eth0 nic nictype=bridged parent=br-c1 name=eth0
 
-snap restart travis-worker
+
